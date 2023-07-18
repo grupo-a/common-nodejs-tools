418f1386ef9ec6337b787cf1ea23ec19c5a3bb50
ehsilva <ehsilva@maisaedu.com.br>
2023-07-18 18:07:37 -0300

    update axios request

--- src/request/index.js
+++ src/request/index.js
@@ -6,10 +6,15 @@ const axios = require('axios');
 
 const handler = (options) => {
   return new Promise((resolve, reject) => {
+    if (options.uri) {
+      options.url = options.uri;
+      delete options.uri;
+    }
+    
     axios(options).then(response => {
       return resolve(response.data);
     }).catch(err => {
-      return reject(err);
+      return reject((err.response && err.response.data) || err);
     });
   });
 };
