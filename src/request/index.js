629703bc69b0e0946eaf182cc4bc0dc3a2c16b43
esato <esato@maisaedu.com.br>
2023-07-21 13:33:43 -0300

    feat: add types to request handler

--- src/request/index.js
+++ src/request/index.js
@@ -4,42 +4,50 @@
 // dependencies
 const axios = require('axios');
 
+const AXIOS_TIMEOUT = 30_000;
+
+/**
+ * @template D - The "data" argument type
+ * @param options {import('axios').AxiosRequestConfig<D>} - Axios options
+ * @returns {Promise<unknown>}
+ */
 const handler = (options) => {
   if (options.uri) {
     options.url = options.uri;
     delete options.uri;
   }
 
-  return axios(options)
-    .then(response => {
-      if (response.data) {
-        if (response.data[0] === '<') {
-          return response.data;
-        }
-        try {
-          return JSON.parse(response.data);
-        } catch {
-          return response.data;
-        }
+  return axios({
+    timeout: AXIOS_TIMEOUT,
+    ...options
+  }).then(response => {
+    if (response.data) {
+      if (response.data[0] === '<') {
+        return response.data;
       }
-
-      return response.data;
-    })
-    .catch(err => {
       try {
-        const data = JSON.parse(err.response.data);
-        data.status = err.response.status;
-        data.status_code = err.response.status;
-        data.status_message = err.response.statusText;
-        return data;
-      } catch (ignored) {
-        return {
-          message: err.response.data,
-          status_code: err.response.status,
-          status_message: err.response.statusText
-        };
+        return JSON.parse(response.data);
+      } catch {
+        return response.data;
       }
-    });
+    }
+
+    return response.data;
+  }).catch(err => {
+    try {
+      const data = JSON.parse(err.response.data);
+      data.status = err.response.status;
+      data.status_code = err.response.status;
+      data.status_message = err.response.statusText;
+      return data;
+    } catch (ignored) {
+      return {
+        message: err.response.data,
+        status_code: err.response.status,
+        status_message: err.response.statusText
+      };
+    }
+  });
 };
 
 module.exports = {
