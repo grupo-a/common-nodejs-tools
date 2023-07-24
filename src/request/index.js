09431cfdfce526d554c24b1b187f629f3e926e5f
esato <esato@maisaedu.com.br>
2023-07-24 17:43:49 -0300

    fix: return custom response when content-type is not json

--- src/request/index.js
+++ src/request/index.js
@@ -7,47 +7,41 @@ const axios = require('axios');
 const AXIOS_TIMEOUT = 30_000;
 
 /**
- * @template D - The "data" argument type
+ * @template D
  * @param options {import('axios').AxiosRequestConfig<D>} - Axios options
  * @returns {Promise<unknown>}
  */
-const handler = (options) => {
+const handler = async (options) => {
   if (options.uri) {
     options.url = options.uri;
     delete options.uri;
   }
 
-  return axios({
-    timeout: AXIOS_TIMEOUT,
-    ...options
-  }).then(response => {
-    if (response.data) {
-      if (response.data[0] === '<') {
-        return response.data;
-      }
-      try {
-        return JSON.parse(response.data);
-      } catch {
-        return response.data;
+  try {
+    const response = await axios({
+      timeout: AXIOS_TIMEOUT,
+      ...options
+    });
+
+    return response.data;
+  } catch (err) {
+    if (!err.response || !err.response.headers['content-type'].startsWith('application/json')) {
+      const { data = null, status = null, statusText = null } = err?.response ?? {};
+      throw {
+        message: data,
+        status: status,
+        status_code: status,
+        status_message: statusText
       }
     }
 
-    return response.data;
-  }).catch(err => {
-    try {
-      const data = JSON.parse(err.response.data);
-      data.status = err.response.status;
-      data.status_code = err.response.status;
-      data.status_message = err.response.statusText;
-      return data;
-    } catch (ignored) {
-      return {
-        message: err.response.data,
-        status_code: err.response.status,
-        status_message: err.response.statusText
-      };
-    }
-  });
+    throw {
+      ...err.response.data,
+      status: err.response.status,
+      status_code: err.response.status,
+      status_message: err.response.statusText
+    };
+  }
 };
 
 module.exports = {
