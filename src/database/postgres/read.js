602caecadf632a4bbf8d0143c9d0e756a14db76e
esato <esato@maisaedu.com.br>
2023-09-24 10:38:38 -0300

    feat: use reject unauthorized and check server identity

--- src/database/postgres/read.js
+++ src/database/postgres/read.js
@@ -6,7 +6,8 @@ const pg = require('pg');
 
 //
 // pool read
-const poolRead = new pg.Pool({
+const noopIdentityCheck = () => {};
+const pgConfig = {
   host              : process.env.DB_READ_HOST,
   port              : process.env.DB_PORT,
   database          : process.env.DB_DATABASE,
@@ -14,7 +15,19 @@ const poolRead = new pg.Pool({
   password          : process.env.DB_PASSWORD,
   statement_timeout : process.env.DB_STATEMENT_TIMEOUT || 0,
   max               : 10
-});
+};
+
+if (process.env.DB_ENABLE_SSL === 'true') {
+  pgConfig.ssl = {
+    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true'
+  }
+
+  if (process.env.DB_SSL_DISABLE_IDENTITY_CHECK === 'true') {
+    pgConfig.ssl.checkServerIdentity = noopIdentityCheck;
+  }
+}
+
+const poolRead = new pg.Pool(pgConfig);
 
 const query = (query, params = []) => {
   return poolRead.query(query, params)
--- src/database/postgres/write.js
+++ src/database/postgres/write.js
@@ -10,15 +10,32 @@ const logger = require('../../logger');
 
 //
 // pool write
-const poolWrite = new pg.Pool({
+const noopIdentityCheck = () => {};
+const pgConfig = {
   host              : process.env.DB_WRITE_HOST,
   port              : process.env.DB_PORT,
   database          : process.env.DB_DATABASE,
   user              : process.env.DB_USER,
   password          : process.env.DB_PASSWORD,
   statement_timeout : process.env.DB_STATEMENT_TIMEOUT || 0,
-  max               : 10
-});
+  max               : 10,
+  ssl: {
+    rejectUnauthorized: true,
+    checkServerIdentity: () => {}
+  }
+};
+
+if (process.env.DB_ENABLE_SSL === 'true') {
+  pgConfig.ssl = {
+    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true'
+  }
+
+  if (process.env.DB_SSL_DISABLE_IDENTITY_CHECK === 'true') {
+    pgConfig.ssl.checkServerIdentity = noopIdentityCheck;
+  }
+}
+
+const poolWrite = new pg.Pool(pgConfig);
 
 let client = null;
 
